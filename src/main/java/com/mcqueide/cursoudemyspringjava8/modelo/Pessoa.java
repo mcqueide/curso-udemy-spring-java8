3a650ccbc6c9ff3dc6e745c98a9a16d8a603a7d3
John Mc.Queide Clemente <mcqueide@gmail.com>
2019-04-28 10:44:53 -0300

    Adicionado teste integração para PessoaRepository

--- src/main/java/com/mcqueide/cursoudemyspringjava8/modelo/Pessoa.java
+++ src/main/java/com/mcqueide/cursoudemyspringjava8/modelo/Pessoa.java
@@ -14,6 +14,7 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -24,6 +25,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
+@Builder
 public class Pessoa {
 
 	@Id
--- src/main/java/com/mcqueide/cursoudemyspringjava8/modelo/Telefone.java
+++ src/main/java/com/mcqueide/cursoudemyspringjava8/modelo/Telefone.java
@@ -9,6 +9,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -19,6 +20,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
+@Builder
 public class Telefone {
 
 	@Id
